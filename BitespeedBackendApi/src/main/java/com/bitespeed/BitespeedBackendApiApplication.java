3d72002dbd9ff66fac2665554c24d532dbdda907
Amruta <amrutatatekar02@gmail.com>
2025-05-18 13:09:15 +0530

    project initialized

---
+++ BitespeedBackendApi/src/main/java/com/bitespeed/BitespeedBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitespeedBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitespeedBackendApiApplication.class, args);
+	}
+
+}
